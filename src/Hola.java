5607c6c52ed481103fb66a591d3f13a54610b867
addolfo <financieras@gmail.com>
2021-06-10 12:01:33 +0200

    Primer commit

---
+++ src/Hola.java
@@ -0,0 +1,6 @@
+//Creación de un Hola Mundo
+public class Hola {
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
+}
---
+++ src/Hola2.java
@@ -0,0 +1,12 @@
+
+public class Hola2 {
+
+	public Hola2() {
+		System.out.print("Hola planeta JAVA");
+	}
+	
+	public static void main(String[] args) {
+		new Hola2();
+	}
+
+}
